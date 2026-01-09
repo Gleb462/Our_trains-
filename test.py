745f5d9c27e7850349173cbf18e984698d0f1958
Gleb <127433967+Gleb462@users.noreply.github.com>
2026-01-10 01:58:58 +0300

    Поезда - поездатые!

---
+++ test.py
@@ -0,0 +1,8 @@
+import os
+import time
+print("Наши поезда самые поездатые поезда в мире!")
+time.sleep(1)
+os.system("clear")
+print("Этот репозиторий или проект создан для изучений возможностей этого GitHub!")
+time.sleep(1)
+input()
